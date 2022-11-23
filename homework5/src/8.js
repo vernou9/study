45a06e994e90cd95622482f37d0eb79f70a5b5ba
vernou9 <114872411+vernou9@users.noreply.github.com>
2022-11-24 02:14:38 +0500

    fc2

--- homework5/src/8.js
+++ homework5/src/8.js
@@ -1,4 +1,6 @@
 let stroka='страшно, очень страшно, мы не знаем что это такое, если бы мы знали, что это такое, но мы не знаем, что это такое';
-let str = stroka.split(' ');
-
-console.log(str);
+let str = Array.from(stroka.split(' '));
+var count = {};
+str.forEach(function(i) { count[i] = (count[i]||0) + 1;});
+console.log(str)
+console.log(count);
